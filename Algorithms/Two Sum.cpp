d0eb386e3855ba0d9afd23de9e413815f98ac72f
继续写代码 <34361498+jerrykcode@users.noreply.github.com>
2018-04-18 09:11:02 +0800

    Create Two Sum.cpp

---
+++ Algorithms/Two Sum.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		for (int i = 0; i < nums.size(); i++) 
+			for (int j = 0; j < nums.size(); j++)
+					if (nums[i] + nums[j] == target && i != j)
+					return {i, j};
+		return {-1, -1};
+	}
+};
