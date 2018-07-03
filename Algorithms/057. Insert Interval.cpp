5216ca4b1494ccb5b5a24780419db986e0401393
继续写代码 <34361498+jerrykcode@users.noreply.github.com>
2018-07-03 16:43:35 +0800

    Create 057. Insert Interval.cpp

---
+++ Algorithms/057. Insert Interval.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+
+bool compare(Interval a, Interval b) {
+    return a.start < b.start;
+}
+
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        intervals.push_back(newInterval);
+        return merge(intervals);
+    }
+private:
+    vector<Interval> merge(vector<Interval>& intervals);
+};
+
+vector<Interval> Solution::merge(vector<Interval>& intervals) {
+    vector<Interval> result;
+    if (intervals.size() == 0)
+        return result;
+    sort(intervals.begin(), intervals.end(), compare);
+    int start = intervals[0].start, end = intervals[0].end;        
+    for (int i = 0; i < intervals.size(); i ++) {
+        if (end >= intervals[i].start) 
+            end = max(end, intervals[i].end);
+        else {
+            Interval interval(start, end);
+            result.push_back(interval);
+            start = intervals[i].start;
+            end = intervals[i].end;
+        }
+    }
+    Interval interval(start, end);
+    result.push_back(interval);
+    return result;
+}
