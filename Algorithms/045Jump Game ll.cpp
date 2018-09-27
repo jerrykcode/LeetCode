439bf940942bf8140fe0b7b2786b39d356252dea
继续写代码 <jerrykang026@gmail.com>
2018-09-28 00:34:31 +0800

    format

--- Algorithms/045Jump Game ll.cpp
+++ Algorithms/045Jump Game ll.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        
-	int n = nums.size();
-	int *dist = new int[n];
-	fill(dist, dist + n, 0);
-	int lastIndex = 0;
-	for (int i = 0; i < n; i++) {
+	int jump(vector<int>& nums) {
+		int n = nums.size();
+		int *dist = new int[n];
+		fill(dist, dist + n, 0);
+		int lastIndex = 0;
+		for (int i = 0; i < n; i++) {
 			if (i + nums[i] <= lastIndex) continue;
-        int j;
-        for (j = lastIndex + 1; j <= i + nums[i] && j < n; j++) {
-			dist[j] = dist[i] + 1;
+			int j;
+			for (j = lastIndex + 1; j <= i + nums[i] && j < n; j++) {
+				dist[j] = dist[i] + 1;
+			}
+			lastIndex = j - 1;
+			if (lastIndex >= n - 1) break;
 		}
-		lastIndex = j - 1;
-        if (lastIndex >= n - 1) break;
+		int result = dist[n - 1];
+		free(dist);
+		return result;
 	}
-	int result = dist[n - 1];
-	free(dist);
-    return result;
-    }
 };
+    
